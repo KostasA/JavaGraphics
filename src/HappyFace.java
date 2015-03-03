446cfa73ac53c482a21e80bdacb3f2edff1b4872
Kostas Anastasiou <konanast@gmail.com>
2015-03-03 20:09:53 +0200

    03 Mar 15 08:09 pm

--- src/HappyFace.java
+++ src/HappyFace.java
@@ -20,7 +20,7 @@ public class HappyFace extends JApplet {
         canvas.drawOval(100,50,200,200);
         canvas.fillOval(155, 100, 10, 20);
         canvas.fillOval(230, 100, 10, 20);
-        canvas.drawArc(150, 160, 100, 50, 0, 180);
+        canvas.drawArc(150, 200, 100, 50, 0, 180);
     }
     
        
